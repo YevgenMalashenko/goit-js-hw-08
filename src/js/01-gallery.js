8720e9899d1d6517024c117f2c190b80380ed35c
YevgenMalashenko <malashenkoyevgen1986@gmail.com>
2022-08-08 20:15:12 +0300

    dlf;ja;s

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryElement = document.querySelector('.gallery');
+
+const itemsToInsert = galleryItems
+  .map(
+    item =>
+      `<div class = "gallery__item"><a class = "gallery__link" href = "${item.original}"><img class = "gallery__image"src = "${item.preview}"alt = "${item.description}"></div>`
+  )
+  .join('');
+
+galleryElement.insertAdjacentHTML('beforeend', itemsToInsert);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: '250',
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+import Player from '@vimeo/player';
 
+require(['https://player.vimeo.com/api/player.js'], function (Player) {
+  const iframe = document.querySelector('iframe');
+  const player = new Player(iframe);
+
+  player.on('play', function () {
+    console.log('played the video!');
+  });
+});
